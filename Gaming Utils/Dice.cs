5d9b2c6f344546271a72eaef1c3cbb59a9d5738e
Chris Medley <chris.medley@EAS.local>
2023-10-26 10:35:14 -0400

    first real commit

---
+++ Gaming Utils/Dice.cs
@@ -0,0 +1,64 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+class Dice
+{
+    public static void RollDice(int[] diceInfoIn)
+    {
+
+        //generate and display the dice rolls
+
+        for (int i = 0; i < diceInfoIn[1]; i++)
+        {
+            Random rand = new Random();
+            int randNum = rand.Next(1, diceInfoIn[0]);
+            Console.WriteLine(randNum);
+        }
+
+
+    }
+
+    public static void RollCharacter()
+    {
+        //Set the descriptors to print out **look into multidimensional array in future for cleaner (if not easier)
+        string[] abilities = { "Strength", "Dexterity", "Constitution", "Inteligence", "Wisdom", "Charisma" };
+
+        //Create & set initial loop
+        bool another = true;
+        while (another == true)
+        {
+            //Generate and list random numbers between 6 & 18
+            //surely there's a better way to put in a clear line
+
+            Console.WriteLine(" ");
+            Console.WriteLine("Your Scores");
+
+            for (int k = 0; k < 6; k++)
+            {
+                int current = k;
+                int[] randNum = new int[6];
+                Random rand = new Random();
+                randNum[current] = rand.Next(6, 19);
+                Console.WriteLine($"{abilities[current]}:{randNum[current]}");
+
+            }
+
+            //Ask if they need MORE POWER!
+            Console.WriteLine("Roll another character? (Affirmative/Yes or Negative/No)");
+            string anotherInput = Console.ReadLine();
+
+            //drop it down flip it and reverse it (actually just make it lower case)
+            anotherInput = anotherInput.ToLower();
+
+            //look into more string & array tools like StartsWith && Console.WriteLineIf
+            if (anotherInput.StartsWith("a") || anotherInput.StartsWith("y"))
+
+                another = true;
+
+            else
+
+                another = false;
+
+            //Insert code here to write it out formated with ability scores pretty
+        }
+    }
+}
---
+++ Gaming Utils/Program.cs
@@ -0,0 +1,59 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+//chose what to call
+Console.WriteLine("Roll Dice or Roll Character?");
+string option = Console.ReadLine();
+option = option.ToLower();
+
+if (option == "roll dice")
+
+{
+    //Set the initial array
+    int[] diceInfoOut = { 0, 0 };
+
+    //Get type of die cleanly
+    Console.WriteLine("How many faces on the die?");
+    if (!int.TryParse(Console.ReadLine(), out diceInfoOut[0]))
+    {
+        Console.WriteLine("Integers only please.");
+        Environment.Exit(0);
+    }
+    else if (diceInfoOut[0] < 2)
+    {
+        Console.WriteLine("Greater than one please.");
+        Environment.Exit(0);
+    }
+
+    //Get how many rolls cleanly
+    Console.WriteLine("How many rolls of the die?");
+    if (!int.TryParse(Console.ReadLine(), out diceInfoOut[1]))
+    {
+        Console.WriteLine("Integers only please.");
+        Environment.Exit(0);
+    }
+    else if (diceInfoOut[1] == 0)
+    {
+        Console.WriteLine("Greater than zero please.");
+        Environment.Exit(0);
+    }
+
+    //JUST ROLL IT!
+
+    Dice.RollDice(diceInfoOut);
+    Console.WriteLine(" ");
+    Console.WriteLine("fin");
+
+}
+
+else if (option == "roll character")
+{
+    Dice.RollCharacter();
+    Console.WriteLine(" ");
+    Console.WriteLine("fin");
+}
+
+else
+
+    Console.WriteLine("Invalid Selection.");
+//do I want to return to 10 here?
